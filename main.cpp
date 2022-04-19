07d00fa325e24beede2ca36f288dedfecbbd58b6
若木杜洋 <jailulelivre017@gmail.com>
2022-04-19 10:00:29 +0900

    空のウィンドウの生成

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <Windows.h>
+#include <tchar.h>
 #ifdef _DEBUG
 #include <iostream>
 #endif
@@ -15,7 +16,7 @@ void DebugOutputFormatString(const char* format, ...)
 #ifdef _DEBUG
 	va_list valist;
 	va_start(valist, format);
-	printf(format, valist);
+	vprintf(format, valist);
 	va_end(valist);
 #endif
 }
@@ -25,19 +26,70 @@ LRESULT WindowProcedure(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 	// ウィンドウが破壊されたら呼ばれる
 	if (msg == WM_DESTROY)
 	{
-
+		PostQuitMessage(0); //OSに終了を伝える
+		return 0;
 	}
+	return DefWindowProc(hwnd, msg, wparam, lparam);
 }
+
 
 #ifdef _DEBUG
 int main()
 {
+	WNDCLASSEX w = {};
+
+	w.cbSize = sizeof(WNDCLASSEX);
+	w.lpfnWndProc = (WNDPROC)WindowProcedure; //コールバック関数の指定
+	w.lpszClassName = _T(" DX 12 Sample");
+	w.hInstance = GetModuleHandle(nullptr);
+
+	RegisterClassEx(&w); //application class
+
+	int window_width = 1600;
+	int window_height = 1000;
+	RECT wrc = { 0, 0, window_width, window_height };//ウィンドウサイズの決定
+
+	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false); //ウィンドウサイズ補正
+
+	//ウィンドウオブジェクトの生成
+	HWND hwnd = CreateWindowW(w.lpszClassName, //クラス名指定
+		_T("DX12TEST"),	//タイトルバーの文字
+		WS_OVERLAPPEDWINDOW, //タイトルバーとの境界線
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		wrc.right - wrc.left,//ウィンドウ幅
+		wrc.bottom - wrc.top,//ウィンドウ高
+		nullptr,				//親ウィンドウハンドル
+		nullptr,				//メニューハンドル
+		w.hInstance,			//呼び出しアプリケーションハンドル
+		nullptr);			//追加パラメーター
+
+	ShowWindow(hwnd, SW_SHOW);
+
+	MSG msg = {};
+
+	while (true)
+	{
+		if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+
+		//アプリケーションが終わるときmessageがWM_QUITになる
+		if (msg.message == WM_QUIT)
+		{
+			break;
+		}
+	}
+
+	UnregisterClass(w.lpszClassName, w.hInstance);
+
 #else
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
 #endif
-	DebugOutputFormatString("Show window test.");
-	getchar();
+	printf("test");
 	return 0;
-	
+
 }
