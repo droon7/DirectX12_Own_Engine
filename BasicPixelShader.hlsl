62d4b402a30c1770d5cfca4abededfc012607a53
droon7 <saplingsapling017@gmail.com>
2023-02-25 16:52:08 +0900

    アンビエントの加算

--- BasicPixelShader.hlsl
+++ BasicPixelShader.hlsl
@@ -3,14 +3,20 @@
 float4 BasicPS(Output input) : SV_TARGET
 {
 	float3 light = normalize(float3(1, -1, 1));
+
+
 	float brightness = dot(-light, input.normal);
-	float2 normalUV = (input.normal.xy + float2(1, -1)) * float2(0.5, -0.5);
+	float4 color = tex.Sample(smp, input.uv);
+	//float2 normalUV = (input.normal.xy + float2(1, -1)) * float2(0.5, -0.5);
 	float2 sphereMapUV = (input.vnormal.xy + float2(1, -1)) * float2(0.5, -0.5);
+
 
 	return float4(brightness, brightness, brightness, 1) 
 			* diffuse 
+			* color
 			* tex.Sample(smp, input.uv)
 			* sph.Sample(smp, sphereMapUV)
 			+ spa.Sample(smp, sphereMapUV)
+			+ float4(color * ambient, 1)
 			;
 }
