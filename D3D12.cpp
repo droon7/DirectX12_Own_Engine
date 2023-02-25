a0ea761070e45f4d2dd9154c9eff95f65e90ccae
droon7 <saplingsapling017@gmail.com>
2023-02-25 21:49:16 +0900

    カメラの設定変更

--- D3D12.cpp
+++ D3D12.cpp
@@ -187,8 +187,8 @@ void Dx12::LoadAssets()
 	//std::string strModelPath = "Model/鏡音リン.pmd";
 	//std::string strModelPath = "Model/巡音ルカ.pmd";
 	//std::string strModelPath = "Model/弱音ハク.pmd";
-	std::string strModelPath = "Model/初音ミク.pmd";
-	//std::string strModelPath = "Model/初音ミクmetal.pmd";
+	//std::string strModelPath = "Model/初音ミク.pmd";
+	std::string strModelPath = "Model/初音ミクmetal.pmd";
 
 	auto err = fopen_s(&fp,strModelPath.c_str(), "rb");
 
@@ -471,15 +471,15 @@ void Dx12::LoadAssets()
 	worldMat = XMMatrixRotationY(0);
 	//matrix = worldMat;
 
-	XMFLOAT3 eye(0, 17, -5); 
-	XMFLOAT3 target(0, 17, 0); // eye座標とtarget座標から視線ベクトルを作る
+	XMFLOAT3 eye(0, 15, -15); 
+	XMFLOAT3 target(0, 15, 0); // eye座標とtarget座標から視線ベクトルを作る
 	XMFLOAT3 up(0, 1, 0);
 
 	viewMat = XMMatrixLookAtLH(XMLoadFloat3(&eye), XMLoadFloat3(&target), XMLoadFloat3(&up));
 	//matrix *= viewMat;
 
 	projMat = XMMatrixPerspectiveFovLH(
-		XM_PIDIV2,
+		XM_PIDIV4,
 		static_cast<float>(window_width) / static_cast<float>(window_height),
 		1.0f,
 		100.0f
