d7d0e21bb36840172e6a230fd284c0257c6c755b
droon7 <saplingsapling017@gmail.com>
2023-03-16 19:53:07 +0900

    色の反転の実装

--- planePixel.hlsl
+++ planePixel.hlsl
@@ -2,8 +2,12 @@
 
 float4 ps(Output input) : SV_TARGET
 {
-	//PAL規格におけるグレースケール変換
 	float4 color = tex.Sample(smp,input.uv);
-	float Y = dot(color.rgb, float3(0.299, 0.587, 0.114));
-	return float4(Y, Y, Y, 1);
+
+	////PAL規格におけるグレースケール変換
+	//float Y = dot(color.rgb, float3(0.299, 0.587, 0.114));
+	//return float4(Y, Y, Y, 1);
+
+	//色の反転
+	return float4(1.0f - color.rgb, color.a);
 }
