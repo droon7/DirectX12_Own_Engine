df0c710da89c669f8f2af66b1846c221a60e6478
droon7 <saplingsapling017@gmail.com>
2023-03-16 19:50:59 +0900

    PAL規格におけるグレースケール変換実装

--- planePixel.hlsl
+++ planePixel.hlsl
@@ -2,6 +2,8 @@
 
 float4 ps(Output input) : SV_TARGET
 {
-
-	return tex.Sample(smp,input.uv);
+	//PAL規格におけるグレースケール変換
+	float4 color = tex.Sample(smp,input.uv);
+	float Y = dot(color.rgb, float3(0.299, 0.587, 0.114));
+	return float4(Y, Y, Y, 1);
 }
